eaab5830e785fcafc98200811d4b1fab1157fe40
AndyKingche <laqm_14@hotmail.com>
2026-03-13 17:02:15 -0500

    feature: se modifico el diseño de las cards news

--- src/app/pages/home/components/news-cards/news-cards.component.ts
+++ src/app/pages/home/components/news-cards/news-cards.component.ts
@@ -21,14 +21,14 @@ export class NewsCardsComponent {
       badge: 'Ya llegó',
       badgeColor: '#B8D5D9',
       title: 'Bases Fit Me Fresh Tint',
-      description: 'Bases de maquillaje de uso diario FPS 50 ✨',
+      description: 'Bases Ligeritas y efectivas ✨',
       image: 'https://pycca.vteximg.com.br/arquivos/ids/296437-600-600/ES0557--2-.png?v=638984737877170000'
     },
     {
       badge: 'NUEVO',
       badgeColor: '#27ae60',
-      title: 'e.l.f. Lip Oil PLUMPING',
-      description: 'Tu lip oil ultrabrillante y nutritivo favorito en tono MAJOR MAUVE se vuelve más voluminoso (Plumping) gracias a e.l.f ✨',
+      title: 'E.l.f. Lip Oil PLUMPING',
+      description: 'Pocos en stock 😮',
       image: 'https://www.elfcosmetics.com/dw/image/v2/BBXC_PRD/on/demandware.static/-/Sites-elf-master/default/dwb5661144/2025/GlowReviverPlumpingLipOil/82224_OpenA_V2_R.jpg?sfrm=png&sw=780&q=90&strip=false'
     },
     {
@@ -37,6 +37,13 @@ export class NewsCardsComponent {
       title: 'Fijador One / Size',
       description: 'Agotado temporalmente 😢',
       image: 'https://www.uhlala.mx/cdn/shop/files/ONE_SIZEbyPatrickStarrrOn_TilDawnMattifyingWaterproofSettingSprayBig_BittyDuo6.jpg?v=1759515268&width=1400'
+    },
+    {
+      badge: 'EN CAMINO',
+      badgeColor: '#B8D5D9',
+      title: 'Fijador Loreal Infallible',
+      description: 'Están  por llegar 😀',
+      image:'https://www.loreal-paris.es/-/media/project/loreal/brand-sites/oap/emea/es/products/makeup/face-makeup/infaillible/infaillible-3-second-setting-mist/headerv2.jpg?cx=0.65&cy=0.51&cw=2000&ch=937&hash=20CFBD29782E51FCD967F41CAF8ECCCD'
     },
     {
       badge: '10% OFF',
