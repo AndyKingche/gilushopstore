cef1cb326fa6de9ca7b669c69ff941320933eac8
AndyKingche <laqm_14@hotmail.com>
2026-04-09 19:02:14 -0500

    se modifico el deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 IP="89.117.72.112"
+PORT=2200
 
 echo "Building..."
 npm run build:ssr
 
 echo "Cleaning server..."
-ssh root@$IP "rm -rf /var/www/gilushop/dist/gilustore/browser/* /var/www/gilushop/dist/gilustore/server/*"
+ssh -p $PORT root@$IP "rm -rf /var/www/gilushop/dist/gilustore/browser/* /var/www/gilushop/dist/gilustore/server/*"
 
 echo "Uploading browser files..."
-scp dist/gilustore/*.js root@$IP:/var/www/gilushop/dist/gilustore/browser/
-scp dist/gilustore/*.css root@$IP:/var/www/gilushop/dist/gilustore/browser/
-scp dist/gilustore/*.html root@$IP:/var/www/gilushop/dist/gilustore/browser/
-scp dist/gilustore/*.txt root@$IP:/var/www/gilushop/dist/gilustore/browser/
-scp dist/gilustore/*.xml root@$IP:/var/www/gilushop/dist/gilustore/browser/
-scp -r dist/gilustore/assets root@$IP:/var/www/gilushop/dist/gilustore/browser/
-scp dist/gilustore/gilu-favicon.ico root@$IP:/var/www/gilushop/dist/gilustore/browser/
+scp -P $PORT dist/gilustore/*.js root@$IP:/var/www/gilushop/dist/gilustore/browser/
+scp -P $PORT dist/gilustore/*.css root@$IP:/var/www/gilushop/dist/gilustore/browser/
+scp -P $PORT dist/gilustore/*.html root@$IP:/var/www/gilushop/dist/gilustore/browser/
+scp -P $PORT dist/gilustore/*.txt root@$IP:/var/www/gilushop/dist/gilustore/browser/
+scp -P $PORT dist/gilustore/*.xml root@$IP:/var/www/gilushop/dist/gilustore/browser/
+scp -P $PORT -r dist/gilustore/assets root@$IP:/var/www/gilushop/dist/gilustore/browser/
+scp -P $PORT dist/gilustore/gilu-favicon.ico root@$IP:/var/www/gilushop/dist/gilustore/browser/
 
 echo "Uploading server files..."
-scp -r dist/gilustore/server/* root@$IP:/var/www/gilushop/dist/gilustore/server/
+scp -P $PORT -r dist/gilustore/server/* root@$IP:/var/www/gilushop/dist/gilustore/server/
 
 echo "Restarting service..."
-ssh root@$IP "systemctl restart gilushop"
+ssh -p $PORT root@$IP "systemctl restart gilushop"
 
 echo "Done!"
